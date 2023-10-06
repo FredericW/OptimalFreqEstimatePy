5d7dd2e8256791daa67ebf4c33c4681ed90f8299
Fei Wei <weifei086@gmail.com>
2023-10-06 17:43:57 +0800

    change codes

--- synthetic_generate.py
+++ synthetic_generate.py
@@ -44,7 +44,7 @@ def generate_synthetic_data(data_type="GAUSSIAN",
 
 
 
-def generate_perturbed_pool(M, a_grid, N_pool):
+def M_to_pool(M, a_grid, N_pool):
     _,dx =M.shape
     perturbed_pool = np.zeros((N_pool,dx))
     for j in range(dx):
@@ -57,33 +57,41 @@ def generate_perturbed_pool(M, a_grid, N_pool):
 
 
 
-def DP_dist_estimation(histo_true, bin_idxs, est_type, eps, repeat, q_est):
-    bins = np.size(bin_idxs)
-    q_true = histo_true/np.sum(histo_true)
+def DP_dist_estimation(data, bin_max,d_est, est_type, eps, repeat, q_est):
+    # set the bins for histogram
+    beta=1
+    bin_idxs_est = np.linspace(beta/d_est/2, beta-beta/d_est/2, d_est)
+
+    # obtain the histogram and frequency
+    histo_true_est,_ = np.histogram(a=data, range=(0,bin_max), bins=d_est)
+    q_true = histo_true_est/np.sum(histo_true_est)
+
+    # generate the transition prbability matrix
     N_pool = 100000
     if est_type == 'sw':
-        a_grid, M = utilities.square_wave(eps,bin_idxs)
+        a_grid, M = utilities.square_wave(eps,bin_idxs_est)
     elif est_type == 'grr':
-        a_grid, M = utilities.general_rr(eps,bin_idxs)
+        a_grid, M = utilities.general_rr(eps,bin_idxs_est)
     elif est_type=='aaa':
-        a_grid=bin_idxs
+        a_grid=bin_idxs_est
         _, sol = opt_variance(eps, a_grid, q_est)
         if sol.success == True:
-            M = np.reshape(sol.x,(bins,bins))
+            M = np.reshape(sol.x,(d_est,d_est))
     else:
         raise NotImplementedError('invalid est_type!')
-
-    perturbed_pool = generate_perturbed_pool(
+    
+    # generate random pool from the matrix
+    perturbed_pool = M_to_pool(
         M=M, a_grid=a_grid, N_pool=N_pool)
     
-    mse_est = utilities.M_to_var(M,a_grid,bin_idxs,q_true)
+    mse_est = utilities.M_to_var(M,a_grid,bin_idxs_est,q_true)
 
     wass_est = 0
-    q_est = np.zeros((1,histo_true.size))
+    q_est = np.zeros((1,d_est))
     for _ in np.arange(repeat):
         data_perturbed=[]
-        for i in np.arange(len(histo_true)):
-            temp = np.random.choice(a = perturbed_pool[:,i], size = histo_true[i])
+        for i in np.arange(len(histo_true_est)):
+            temp = np.random.choice(a = perturbed_pool[:,i], size = histo_true_est[i])
             data_perturbed = np.concatenate((data_perturbed,temp))
         _,hist_perturbed = np.unique(data_perturbed,return_counts=True)
         if est_type!="aaa":
